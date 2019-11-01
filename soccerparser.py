ef2401223190ca61f8ab21cab5f4fdbfa86c6c36
Brad Johnson <climatebrad@gmail.com>
2019-11-01 11:27:29 -0400

    teams loaded into sp.results_df

--- soccerparser.py
+++ soccerparser.py
@@ -1,3 +1,12 @@
+"""Soccer parser.
+
+Usage:
+from soccerparser import SoccerParser
+
+sp = SoccerParser('database.sqlite')
+sp.parse()
+"""
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -6,16 +15,28 @@ import sqlite3
 class SoccerParser:
     """Parses soccer match information."""
     def __init__(self, database):
-        self.con = sqlite3.connect(database)
-        self.df = None
+        self._con = sqlite3.connect(database)
+        self.match_df = None
+        self.teams = None
+        self.results_df = None
 
-    def parse(self)
-        self.df = self._load_matches()
+    def __del__(self):
+        self._con.close()
+
+    def parse(self):
+        """Main function."""
+        self._load_matches()
+        self._load_teams()
+
+    def _load_teams(self):
+        """Loads list of teams into teams."""
+        self.teams = list(np.unique(self.match_df[["HomeTeam", "AwayTeam"]].values.ravel('F')))
+        self.results_df = pd.DataFrame(self.teams, columns=['team'])
 
     def _load_matches(self):
         """Loads 2011 matches into df."""
-        df = pd.read_sql("SELECT * FROM Matches", con, parse_dates=['Date'])
-        self.df = df[df.Season == 2011]
+        df = pd.read_sql("SELECT * FROM Matches", self._con, parse_dates=['Date'])
+        self.match_df = df[df.Season == 2011]
 
     def game_goals(self, team, home_or_away):
         """returns pd.Series with goals scored by team; home_or_away is 'H' or 'A'"""
@@ -27,8 +48,10 @@ class SoccerParser:
             raise ValueError(f"Invalid value {home_or_away}. Must be 'H' or 'A'.")
         gamecol = 'FT' + home_or_away + 'G'
         team_esc = team.replace("'","\\'")
-        return pd.Series(self.df.query(f"{teamcol}=='{team_esc}'")[gamecol])
+        return pd.Series(self.match_df.query(f"{teamcol} == '{team_esc}'")[gamecol])
 
     def total_goals(self, team):
         """returns total goals scored by team"""
-        return game_goals(self.df, team, 'H').append(game_goals(self.df, team, 'A')).sum()
+        home_goals = game_goals(self.match_df, team, 'H')
+        away_goals = game_goals(self.match_df, team, 'A')
+        return home_goals.append(away_goals).sum()
