fcf8c4b9fd83575eca9140358251fa7f0ed02122
Brad Johnson <climatebrad@gmail.com>
2019-11-01 15:29:49 -0400

    match_df table fully populated

--- soccerparser.py
+++ soccerparser.py
@@ -17,7 +17,7 @@ def esc(string):
     """utility function that escapes single quotes"""
     return string.replace("'", "\\'")
 
-def ftr_to_winloss(ftr, is_home):
+def ftr_to_winloss(ftr, is_home: bool):
     """utility function that converts away/home/draw match results to win/loss/draw"""
     if ftr == 'D':
         return ftr
@@ -27,22 +27,32 @@ def ftr_to_winloss(ftr, is_home):
 
 class SoccerParser:
     """Parses soccer match information."""
+    match_cols = ['match_id',
+                       'team',
+                       'opponent',
+                       'date',
+                       'div',
+                       'goals',
+                       'opp_goals',
+                       'result',
+                       'is_home']
+
+    ha_dict = { True: { 'teamcol' : 'HomeTeam',
+                       'oppcol' : 'AwayTeam',
+                       'teamgoals' : 'FTHG',
+                       'oppgoals' : 'FTAG' },
+                False: { 'teamcol' : 'AwayTeam',
+                        'oppcol' : 'HomeTeam',
+                        'teamgoals' : 'FTAG',
+                        'oppgoals' : 'FTHG' }
+              }
 
     def __init__(self, database):
         self._con = sqlite3.connect(database)
         self.input_df = None
         self.teams = None
         self.results_df = None
-        self.match_cols = ['match_id',
-                           'team',
-                           'opponent',
-                           'date',
-                           'div',
-                           'goals',
-                           'opp_goals',
-                           'result',
-                           'is_home']
-        self.match_df = pd.DataFrame(columns=self.match_cols)
+        self.match_df = pd.DataFrame(columns=SoccerParser.match_cols)
 
     def __del__(self):
         self._con.close()
@@ -64,18 +74,29 @@ class SoccerParser:
 
     def _load_team_home_or_away_matches(self, team, is_home: bool):
         """Called by _load_team_matches"""
-        teamcat = 'HomeTeam' if is_home else 'AwayTeam'
-        oppcat = 'AwayTeam' if is_home else 'HomeTeam'
-        localdf = self.input_df.query(f"{teamcat} == '{esc(team)}'").copy()
-        localdf = localdf.rename(columns={teamcat : 'team',
-                                          oppcat : 'opponent'})
-        localdf = localdf.rename(columns=lambda x: x.lower())
+        teamcol = SoccerParser.ha_dict[is_home]['teamcol']
+
+        oppcol = SoccerParser.ha_dict[is_home]['oppcol']
+        localdf = self.input_df.query(f"{teamcol} == '{esc(team)}'").copy()
+
+        # Standardize column names
+        localdf = localdf.rename(columns={teamcol : 'team',
+                                          oppcol : 'opponent'})
+        localdf.columns = localdf.columns.str.lower()
+
+        # Create is_home boolean column
         localdf['is_home'] = is_home
+
+        # Drop the season column
         localdf = localdf.drop(columns='season')
-        localdf.ftr = localdf.ftr.apply(ftr_to_winloss, args=[True])
-        localdf = localdf.rename(columns={'fthg' : 'goals',
-                                          'ftag' : 'opp_goals'})
-        self.match_df[self.match_cols].append(localdf[self.match_cols], ignore_index=True)
+        localdf.ftr = localdf.ftr.apply(ftr_to_winloss, is_home=is_home)
+
+        teamgoals = SoccerParser.ha_dict[is_home]['teamgoals'].lower()
+        oppgoals = SoccerParser.ha_dict[is_home]['oppgoals'].lower()
+        localdf = localdf.rename(columns={'ftr' : 'result',
+                                          teamgoals : 'goals',
+                                          oppgoals : 'opp_goals'})
+        self.match_df = self.match_df[SoccerParser.match_cols].append(localdf[SoccerParser.match_cols], ignore_index=True)
 
     def _load_team_matches(self, team):
         """Loads match results of team into match_df"""
@@ -99,21 +120,16 @@ class SoccerParser:
         """Loads total wins by each team into results_df."""
         self.results_df['wins'] = self.results_df.team.apply(self.team_total_wins)
 
-    def team_game_goals(self, team, home_or_away):
-        """returns pd.Series with goals scored by team; home_or_away is 'H' or 'A'"""
-        if home_or_away == 'H':
-            teamcol = 'HomeTeam'
-        elif home_or_away == 'A':
-            teamcol = 'AwayTeam'
-        else:
-            raise ValueError(f"Invalid value {home_or_away}. Must be 'H' or 'A'.")
-        gamecol = 'FT' + home_or_away + 'G'
-        return pd.Series(self.input_df.query(f"{teamcol} == '{esc(team)}'")[gamecol])
+    def team_game_goals(self, team, is_home: bool):
+        """returns pd.Series with goals scored by team"""
+        teamgoals = SoccerParser.ha_dict[is_home]['teamgoals']
+        teamcol = SoccerParser.ha_dict[is_home]['teamcol']
+        return pd.Series(self.input_df.query(f"{teamcol} == '{esc(team)}'")[teamgoals])
 
     def team_total_goals(self, team):
         """returns total goals scored by team"""
-        home_goals = self.team_game_goals(team, 'H')
-        away_goals = self.team_game_goals(team, 'A')
+        home_goals = self.team_game_goals(team, is_home=True)
+        away_goals = self.team_game_goals(team, is_home=False)
         return home_goals.append(away_goals).sum()
 
     def team_total_wins(self, team):
